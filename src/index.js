ef9fdc28f5b4b08052ac65c7157717b2efcad273
benderTheCrime <joseph.groseclose@spongecell.com>
2015-10-30 07:40:30 -0400

    Added fixes

--- src/index.js
+++ src/index.js
@@ -43,8 +43,18 @@ function pollForExposedServerUpdate() {
     setImmediate(pollForExposedServerUpdate);
 }
 
+// TODO Fix Injector
+    // Remove registry stuff DONE
+    // README Explaination of why it can't be used generically DONE
+    // Special cases for request response and scope DONE
+    // Expose scope $$fetch, request $$fetch and response $$fetch as modules
+// TODO setup simple sessions
+    // Cookie service -> Should be able to manage the session cookie
+    // Need to alias fetch to be provided when scope is called
+// TODO instantiate and store $scope on sessions
+    // Change scope fetcher to retrieve $scope from the session
 // TODO make directives persist to frontend if they are supposed to observe
-    // Make ngie-value directive
+    // Make ngie-value directive -> put in directive specific service
     // Add "ngie-id" as uuid or something equivalent to keep track of databound entities
     // $compile should recognize when a scoped property is bound
 // TODO perform mutation observance
